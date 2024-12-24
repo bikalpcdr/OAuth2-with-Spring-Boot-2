f7c50cbf30b22af8346a2a3189b3ce811e581652
bikalpcdr <bikalpcdr42@gmail.com>
2024-12-24 11:45:27 +0545

    initiatied the project

---
+++ src/main/java/com/backend/OAuth2/with/Spring/Boot3/OAuth2WithSpringBoot3Application.java
@@ -0,0 +1,13 @@
+package com.backend.OAuth2.with.Spring.Boot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2WithSpringBoot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2WithSpringBoot3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/OAuth2/with/Spring/Boot3/OAuth2WithSpringBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.OAuth2.with.Spring.Boot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2WithSpringBoot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
